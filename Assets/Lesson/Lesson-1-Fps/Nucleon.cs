f63873ab87ea2a26f3a419ce61f3cbd39eca8919
EngineVulkan <962114195@qq.com>
2020-03-08 14:00:03 +0800

    Lesson-1-FPS实现

---
+++ Assets/Lesson/Lesson-1-Fps/Nucleon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
